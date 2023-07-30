dc2294671e8516d571178f7853540d346ecac5fb
bekturb <bekkgboy2@gmail.com>
2023-07-30 11:14:06 +0600

    added images fized moments

--- middlewares/multer.js
+++ middlewares/multer.js
@@ -1,7 +1,8 @@
+const fs = require("fs");
 const multer = require("multer");
 const sharp = require("sharp");
 const path = require("path");
-const fs = require("fs");
+
 const storage = multer.diskStorage({
     destination: function (req, file, cb) {
         cb(null, path.join(__dirname, "../public/images/"));
@@ -30,12 +31,14 @@ const productImgResize = async (req, res, next) => {
     if (!req.files) return next();
     await Promise.all(
         req.files.map(async (file) => {
+            const outputPath = path.join(__dirname, "../public/images/", "temp-" + file.filename);
             await sharp(file.path)
                 .resize(300, 300)
                 .toFormat("jpeg")
                 .jpeg({ quality: 90 })
-                .toFile(`public/images/products/${file.filename}`);
-            fs.unlinkSync(`public/images/products/${file.filename}`);
+                .toFile(outputPath);
+            fs.unlinkSync(file.path);
+            fs.renameSync(outputPath, file.path);
         })
     );
     next();
@@ -45,14 +48,17 @@ const blogImgResize = async (req, res, next) => {
     if (!req.files) return next();
     await Promise.all(
         req.files.map(async (file) => {
+            const outputPath = path.join(__dirname, "../public/images/", "temp-" + file.filename);
             await sharp(file.path)
                 .resize(300, 300)
                 .toFormat("jpeg")
                 .jpeg({ quality: 90 })
-                .toFile(`public/images/blogs/${file.filename}`);
-            fs.unlinkSync(`public/images/blogs/${file.filename}`);
+                .toFile(outputPath);
+            fs.unlinkSync(file.path);
+            fs.renameSync(outputPath, file.path);
         })
     );
     next();
 };
+
 module.exports = { uploadPhoto, productImgResize, blogImgResize };

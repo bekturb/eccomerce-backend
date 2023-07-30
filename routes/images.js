66d4b4028c02270fd593ecc51814a0652430f323
bekturb <bekkgboy2@gmail.com>
2023-07-30 09:56:47 +0600

    added some line of codes

--- routes/images.js
+++ routes/images.js
@@ -5,7 +5,7 @@ const { uploadPhoto, productImgResize } = require("../middlewares/multer");
 const auth = require("../middlewares/auth");
 const seller = require("../middlewares/seller");
 
-router.post("/images", [auth, seller], uploadPhoto.array("images"), productImgResize, PhotoController.create);
+router.post("/images", uploadPhoto.array("images"), productImgResize, PhotoController.create);
 router.delete("/delete-images/:id", [auth, seller], PhotoController.deleteImages);
 router.post("/single-image", uploadPhoto.single("image"), productImgResize, PhotoController.uploadSingleImage);
 router.delete("/single-image/:id", PhotoController.deleteImage);

a2e31f5d2252a8111f452106aec281389ac6dea1
bekturb <bekkgboy2@gmail.com>
2023-08-09 20:01:19 +0600

    added resend otp

--- controllers/userController.js
+++ controllers/userController.js
@@ -89,8 +89,6 @@ class UserController {
         if (!OTP)
             return res.status(400).send('Invalid OTP number');
 
-        console.log(OTP)
-
         const isMatch = await bcrypt.compare(req.body.otp, OTP.otp);
 
         if (!isMatch) {
@@ -107,6 +105,52 @@ class UserController {
         } else {
             res.status(400).send("Incorrect OTP or it has been expired.");
         }
+    }
+
+    async resendOtp(req, res) {
+
+        function validateEmail(email) {
+            const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+            return re.test(String(email).toLowerCase());
+        }
+
+        function validateNumber(email) {
+            const phonePattern = /^\+(?:[0-9] ?){6,14}[0-9]$/;
+
+            if (phonePattern.test(email) && !isNaN(email)) {
+                return true;
+            }
+            return false;
+        }
+
+        if (!mongoose.Types.ObjectId.isValid(req.params.id))
+            return res.status(404).send("Invalid Id");
+
+        let user = await User.findById(req.params.id)
+            .select({hash_password: 0});
+        if (!user) return res.status(404).send({message: "No user for the given Id"});
+
+        const OTP = otpGenerator.generate(6, {
+            digits: true, lowerCaseAlphabets: false, upperCaseAlphabets: false, specialChars: false
+        });
+
+        if (validateEmail(user.email)) {
+            await sendEmail(user.email, "Verify your email", `Your OTP is ${OTP}.\nDo not share with anyone`);
+            await Otp.create({otp: OTP, userId: user._id});
+        } else if (validateNumber(user.email)) {
+            client.messages
+                .create({
+                    body: `Your OTP is ${OTP}.\nDo not share with anyone`,
+                    from: process.env.TWILLIO_PHONE_NUMBER,
+                    to: req.body.email
+                }).then(() => {
+                Otp.create({otp: OTP, userId: user._id});
+            })
+        } else {
+            return res.status(400).send("Invalid phone/email.");
+        }
+
+        return res.status(201).send("OTP sent. Valid for only 2 minutes");
     }
 
     async getAll(req, res) {

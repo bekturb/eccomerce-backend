d247037ad36a2e29ae024cab712b13ad173917ad
bekturb <bekkgboy2@gmail.com>
2023-09-09 16:13:27 +0600

    added cron shedule

--- index.js
+++ index.js
@@ -1,12 +1,24 @@
-require("dotenv").config()
+require("dotenv").config();
 const express = require('express');
+const cron = require('node-cron');
 const app = express();
 const winston = require('winston');
+const Events = require("twilio/lib/rest/Events");
 require("./startup/logging")();
 require('./startup/routes')(app);
 require('./startup/db')();
 require('./startup/config')();
 require("./startup/prod")(app);
+
+cron.schedule('0 * * * *', async () => {
+    try {
+        const currentDate = new Date();
+        await Events.deleteMany({ endDate: { $lte: currentDate } });
+        console.log('Expired offers removed.');
+    } catch (error) {
+        console.error('Error removing expired offers:', error);
+    }
+});
 
 const port = process.env.PORT || 5000;
 const localAddress = process.env.LOCAL_ADDRESS

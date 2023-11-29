fce7eee11242fbaeeb5371a31a41638e90a7449d
Baimamatov Bektursun <baimamatovbektursun@MacBook-Pro-Baimamatov.local>
2023-11-29 14:57:10 +0600

    added some user wishlist items

--- controllers/productController.js
+++ controllers/productController.js
@@ -328,7 +328,7 @@ class ProductController {
                 {
                     new: true,
                 });
-            res.status(201).send(user)
+            res.status(201).send(user.wishList)
         }
     }
     async getPersonalWishList(req, res) {

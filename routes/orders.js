0e9a3c50947757a1e092d49e4787486dc4b46f76
bekturb <bekkgboy2@gmail.com>
2024-05-09 12:48:40 +0600

    added auth and seller middlewares in gettinh shop orders

--- routes/orders.js
+++ routes/orders.js
@@ -7,7 +7,7 @@ const seller = require("../middlewares/seller");
 
 router.post("/create-order", orderController.create);
 router.get("/get-all-orders/:userId", orderController.getAll);
-router.get("/get-seller-all-orders/:shopId", orderController.getSellerOrders);
+router.get("/get-seller-all-orders/:shopId", [auth, seller], orderController.getSellerOrders);
 router.put("/update-order-status/:id", [auth, seller], orderController.updateStatus);
 router.put("/order-refund/:id", [auth], orderController.orderRefund);
 router.put("/order-refund-success/:id", [auth, seller], orderController.orderRefundSuccess);

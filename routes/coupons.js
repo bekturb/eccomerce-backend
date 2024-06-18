9985fe79b4f3baaa2cbe1858155eee99c895e225
bekturb <bekkgboy2@gmail.com>
2024-06-18 08:19:50 +0600

    added update coupon route among routes

--- routes/coupons.js
+++ routes/coupons.js
@@ -7,6 +7,7 @@ const seller = require("../middlewares/seller");
 router.post("/", [auth, seller], CouponController.create);
 router.get("/get-all", [auth, seller], CouponController.getAll);
 router.get("/get-all-shop-coupons", [auth, seller], CouponController.getShopCoupons);
+router.get("/update-coupon/:couponId", [auth, seller], CouponController.updateCoupon);
 router.get("/get-single-coupon/:couponId", [auth, seller], CouponController.getOne);
 router.get("/get-coupon-value/:name", [auth], CouponController.getCouponValue);
 router.delete("/delete/:id", [auth, seller], CouponController.delete);

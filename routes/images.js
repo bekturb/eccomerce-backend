ef59c7ab5575b6edd38858603c14843a74627ecb
bekturb <bekkgboy2@gmail.com>
2024-07-06 14:41:22 +0600

    added deleting images

--- routes/images.js
+++ routes/images.js
@@ -3,10 +3,9 @@ const router = express.Router();
 const PhotoController = require('../controllers/imagesController');
 const { uploadPhoto, productImgResize } = require("../middlewares/multer");
 const auth = require("../middlewares/auth");
-const seller = require("../middlewares/seller");
 
 router.post("/images", uploadPhoto.array("images"), productImgResize, PhotoController.create);
-router.delete("/delete-images/:id", [auth, seller], PhotoController.deleteImages);
+router.delete("/delete-images/:id", [auth], PhotoController.deleteImages);
 router.post("/single-image", uploadPhoto.single("image"), productImgResize, PhotoController.uploadSingleImage);
 router.delete("/single-image/:id", PhotoController.deleteImage);
 module.exports = router;
